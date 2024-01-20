ef5fef5f9f96c0c4f885ffbb10f3356641209265
luis ore <luis.ore.cerna@gmail.com>
2024-01-19 21:33:29 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+let persona1 = {
+    nombre: "son goku",
+    profesion : "guerrero",
+    raza : "sayajin",
+    estado_civil : "casado",
+    trabajo: "granjero",
+}
+
+let persona2 = {
+    nombre : "vegeta",
+    profesion : "guerrero",
+    estado_civil : "casado",
+    trabajo : "sin trabajo XD",
+    raza : "sayajin",
+}
+
+let persona3 = {
+    nombre : " son gohan",
+    profesion : "investigador",
+    estado_civil : "casado",
+    trabajo : "cientifico",
+    raza : "hibrido",
+}
+
+let persona4 = {
+    nombre : "tapion",
+    profesion : "guerrero",
+    estado_civil : "soltero",
+    trabajo : "guardian de hildegar",
+    raza : "alienigena",
+}
+
+let persona5 = {
+    nombre : "Mr.satan",
+    profesion : "ARTISTA marcial",
+    estado_civil : "viudo",
+    trabajo : "influecer",
+    raza : "humano",
+}
+
+console.log(persona1.nombre)
+console.log(persona2.estado_civil)
+console.log(persona3.profesion)
+console.log(persona4.raza)
+console.log(persona5.trabajo)
+
+
